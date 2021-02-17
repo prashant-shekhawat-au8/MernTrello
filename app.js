a394b844979dfe254fe63e3e1cadc1934d56c4bc
Prashant Shekhawat <prashantshekhawat100@gmail.com>
2021-02-17 17:29:53 +0530

    done

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express=require("express")
 const app=express()
 const mongoose=require('mongoose');
 const dotenv=require('dotenv')
+dotenv.config();
 const port=process.env.PORT||5004
 //import route
 const authRoute=require('./routes/auth.js')
 const taskRoute=require('./routes/task.js')
 
-dotenv.config();
+
 
 mongoose.connect(process.env.DB_CONNECT,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false } ,()=>console.log("connected to database"))
 
